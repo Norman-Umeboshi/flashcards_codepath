24e1505103e105db5a1814c5fd218065cf56157c
Norman-Umeboshi <normanta405@gmail.com>
2021-02-22 21:23:13 -0800

    added README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+## Codepath: Flashcards
+
+## Lab 1
+
+### App Description
+This app acts as a virtual flashcard. It has a question on one side and the answer on the other side.
+
+### App Walk-though
+
+<img src="https://i.imgur.com/Kbfk31L.gif" width=200><br>
+
+
+## Required
+- [x] Create New Project in Android Studio
+- [x] Add a view for the front side of the flashcard to display the question
+- [x] Add a view for the back side of the flashcard to display the answer
+- [x] Build in logic to show the answer side when the card is tapped
+- [x] Push code to GitHu
+## Optional
+- [x] toggle the flashcard between the question side and the answer side
+- [x] Style the question and answer side of the card to better distinguish between the two sides
+- [ ] Add selectable multiple choice answers beneath the card
+   - [ ] Change the background color of the multiple choice answers when clicked to indicate whether the question waanswered correctly
+- [ ] Further customize and style the card
--- app/src/main/java/com/example/flashcard_nt/MainActivity.java
+++ app/src/main/java/com/example/flashcard_nt/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.flashcard_nt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView flashcardATXTView = (TextView) findViewById(R.id.flashcardA);
+        TextView flashcardQTXTView = (TextView) findViewById(R.id.flashcardQ);
+        flashcardATXTView.setVisibility(View.INVISIBLE);
+        findViewById(R.id.flashcardQ).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardQTXTView.setVisibility(View.INVISIBLE);
+                flashcardATXTView.setVisibility(View.VISIBLE);
+            }
+        });
+        findViewById(R.id.flashcardA).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardQTXTView.setVisibility(View.VISIBLE);
+                flashcardATXTView.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
